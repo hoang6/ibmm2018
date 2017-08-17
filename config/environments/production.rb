88e6b2c68fe77061e405cf006c8e2113c2966bb0
TLH <tuan.l.hoang@gmail.com>
2017-08-16 21:38:08 -0500

    fix css background-image issue on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Fix css background-image loading issue with Heroku
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
