0bb00edbdc193fdd902988cb261b2162e97dc01d
TLH <tuan.l.hoang@gmail.com>
2017-10-06 10:09:29 -0500

    Change registration link

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -68,4 +68,8 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not @user.valid?
   end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
